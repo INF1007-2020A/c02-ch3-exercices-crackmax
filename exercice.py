1e56dcecaee932484c3c0e5791ffcc6712aeab7a
maxpichet <60222646+crackmax@users.noreply.github.com>
2020-09-14 14:04:25 -0400

    fin

--- exercice.py
+++ exercice.py
@@ -3,32 +3,48 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
+	power = voltage**2 / resistance
 	return 0
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
 	v1[0] # Pour accéder au X
 	v1[1] # Pour accéder au Y
-	pass
+	# Calculer le produit scalaire
+	dot_product = v1[0] * v2[0] + v1[1] * v2[1]
+	#verifier si dot_product=0
+	#retoruner vrai si = 0, faux sinon
+	return dot_product == 0
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	sum = 0
+	num_values =0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v >= 0:
+			sum  += v
+			num_values+=1
 
+	return sum/ num_values
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+	twenties = tens = fives = ones = 0
 	while value != 0:
 		if value >= 20:
-			pass
-		elif value >= 10:
-			pass
-		elif value >= 5:
-			pass
-		elif value >= 1:
-			pass
+			twenties = value // 20
+			value = value % 20
 
-	return (twenties, tens, fives, twos, ones);
+		elif value >= 10:
+			tens = value // 10
+			value = value % 10
+		elif value >= 5:
+			fives = value // 5
+			value = value % 5
+		elif value >= 1:
+			ones += value
+			value =0
+
+	return (twenties, tens, fives, ones)
 
 if __name__ == "__main__":
 	print(dissipated_power(69, 420))
